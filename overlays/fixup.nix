f1f67ff9e57a4ac18bb80184348c75e43438904a
Basile Henry <bjm.henry@gmail.com>
2020-11-01 09:38:45 +0100

    mosh on NixOS 20.09

--- overlays/fixup.nix
+++ overlays/fixup.nix
@@ -6,10 +6,11 @@ self: super:
     enableVaapi = true;
   });
 
-  # mosh from nixos-20.03 doesn't include a colour fix
+  # mosh from nixos-20.09 doesn't include a colour fix
   mosh = super.mosh.overrideAttrs (attrs: {
     patches = [
       (builtins.elemAt attrs.patches 0) # ssh patch
+      (builtins.elemAt attrs.patches 3) # bash completion patch
     ];
 
     src = builtins.fetchGit {
