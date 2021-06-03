ff72e9b53ae4ce39a4949ab5885f86572c15128f
Basile Henry <bjm.henry@gmail.com>
2021-06-03 10:54:55 +0200

    Move vscode

--- gui.nix
+++ gui.nix
@@ -2,26 +2,11 @@
 
 let unstable = import <nixpkgs-unstable> {};
 
-    extensions = (with unstable.vscode-extensions; [
-      bbenoist.Nix
-      ms-python.python
-      ms-vsliveshare.vsliveshare
-      vscodevim.vim
-      matklad.rust-analyzer
-      alanz.vscode-hie-server
-    ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-        name = "errorlens";
-        publisher = "usernamehw";
-        version = "3.2.4";
-        sha256 = "0caxmf6v0s5kgp6cp3j1kk7slhspjv5kzhn4sq3miyl5jkrn95kx";
-    }];
-    vscode-with-extensions = pkgs.vscode-with-extensions.override {
-      vscodeExtensions = extensions;
-    };
 in
 {
   imports = [
     ./program/alacritty.nix
+    ./program/vscode.nix
     "${fetchTarball "https://github.com/msteen/nixos-vsliveshare/tarball/master"}/modules/vsliveshare/home.nix"
   ];
 
@@ -43,7 +28,6 @@ in
     hexchat
     obs-studio
 
-    vscode-with-extensions
 
     # Games
     steam
---
+++ program/vscode.nix
@@ -0,0 +1,35 @@
+
+{ config, lib, pkgs, ... }:
+
+let unstable = import <nixpkgs-unstable> {};
+
+    vscode-with-extensions = pkgs.vscode-with-extensions.override {
+      vscodeExtensions = (with unstable.vscode-extensions; [
+        bbenoist.Nix
+        ms-python.python
+        ms-vsliveshare.vsliveshare
+        ms-vscode-remote.remote-ssh
+        ms-vscode.cpptools
+        vscodevim.vim
+        matklad.rust-analyzer
+        haskell.haskell
+      ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+          name = "errorlens";
+          publisher = "usernamehw";
+          version = "3.2.4";
+          sha256 = "0caxmf6v0s5kgp6cp3j1kk7slhspjv5kzhn4sq3miyl5jkrn95kx";
+        }
+        {
+          name = "language-haskell";
+          publisher = "justusadam";
+          version = "3.3.0";
+          sha256 = "1285bs89d7hqn8h8jyxww7712070zw2ccrgy6aswd39arscniffs";
+        }
+      ];
+    };
+in
+{
+  home.packages = [
+    vscode-with-extensions
+  ];
+}
