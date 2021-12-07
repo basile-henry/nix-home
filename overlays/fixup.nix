1f78e5ae0eb7da44f36cdee9c93e74430ca1c532
Basile Henry <bjm.henry@gmail.com>
2021-12-07 16:23:34 +0100

    patch mosh for nixos 21.11

--- overlays/fixup.nix
+++ overlays/fixup.nix
@@ -1,12 +1,18 @@
 self: super:
 
 {
-  # mosh from nixos-20.09 doesn't include a colour fix
+  # mosh from nixos-21.11 doesn't include a colour fix
   mosh = super.mosh.overrideAttrs (attrs: {
     patches = [
       (builtins.elemAt attrs.patches 0) # ssh patch
       (builtins.elemAt attrs.patches 4) # bash completion patch
     ];
+
+    postPatch = ''
+        substituteInPlace scripts/mosh.pl \
+      --subst-var-by ssh "${super.openssh}/bin/ssh" \
+      --subst-var-by mosh-client "$out/bin/mosh-client"
+    '';
 
     src = builtins.fetchGit {
       url = "ssh://git@github.com/mobile-shell/mosh.git";
