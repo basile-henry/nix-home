66c0a5fad08117e30c634c125ac44e5e2097591b
Basile Henry <bjm.henry@gmail.com>
2022-12-14 11:13:18 +0000

    vim: lightline with full git path

--- program/vim.nix
+++ program/vim.nix
@@ -58,7 +58,19 @@ in
 
       let g:lightline = {
         \ 'colorscheme': 'wombat',
+        \ 'component_function': {
+        \   'filename': 'LightlineFilename',
         \ }
+        \ }
+
+      function! LightlineFilename()
+        let root = fnamemodify(get(b:, 'git_dir'), ':h')
+        let path = expand('%:p')
+        if path[:len(root)-1] ==# root
+          return path[len(root)+1:]
+        endif
+        return expand('%')
+      endfunction
 
       let mapleader=" "
 
